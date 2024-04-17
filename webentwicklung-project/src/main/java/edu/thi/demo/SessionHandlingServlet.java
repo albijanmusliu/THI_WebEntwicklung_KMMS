3e768eb2ac468d860918f817f8b813e50bea2d6a
Albijan Musliu <alm3413@thi.de>
2024-04-17 18:33:35 +0200

    ready to use

--- webentwicklung-project/src/main/java/edu/thi/demo/SessionHandlingServlet.java
+++ webentwicklung-project/src/main/java/edu/thi/demo/SessionHandlingServlet.java
@@ -1,91 +1,53 @@
 package edu.thi.demo;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-/**
- * Servlet implementation class SessionHandlingServlet
- */
 @WebServlet("/SessionHandlingServlet")
 public class SessionHandlingServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// HTTP-Header setzen
-		response.setStatus(HttpServletResponse.SC_OK);	// nicht zwingend erforderlich; ist der default-Wert
-		response.setContentType("text/html");
-		response.setCharacterEncoding("UTF-8");
-		
-		final PrintWriter out = response.getWriter();
 		String action = request.getParameter("action");
-		String vorname = null;
-		String nachname = null;
-		HttpSession session = null;
-		
-		switch (action) {
-			case "writeToSession":
-				vorname = request.getParameter("vorname");
-				nachname = request.getParameter("nachname");
-				session = request.getSession();
-				session.setAttribute("vorname", vorname);
-				session.setAttribute("nachname", nachname);
-				
-				out.println("<!DOCTYPE html>");
-				out.println("<html>");
-				out.println("<body>");
-				out.println("<h3>Schreiben von Daten in eine Sitzung</h3>");
-				out.println("Sessiondaten wurden erfolgreich geschrieben!");
-				out.println("<form method='post' action='SessionHandlingServlet'>");
-				out.println("<div>");
-				out.println("<button type='submit' name='action' value='readFromSession'>Daten aus Session lesen</button>");
-				out.println("</div>");
-				out.println("</form>");
-				out.println("</body>");
-				out.println("</html>");
-				break;
-				
-			case "readFromSession":
-				session = request.getSession();
-				vorname = (String) session.getAttribute("vorname");
-				nachname = (String) session.getAttribute("nachname");
-				out.println("<!DOCTYPE html>");
-				out.println("<html>");
-				out.println("<body>");
-				out.println("<h3>Lesen von Daten aus einer Sitzung</h3>");
-				out.println("<b>Gelesene Daten:</b><br>");
-				out.println("Vorname: " + vorname + "<br>");
-				out.println("Nachname: " + nachname + "<br>");
-				out.println("</body>");
-				out.println("</html>");
-				break;
-				
-			default:
-				out.println("<!DOCTYPE html>");
-				out.println("<html>");
-				out.println("<body>");
-				out.println("<h3>Unbekannte Aktion!</h3>");
-				out.println("</body>");
-				out.println("</html>");
-				break;
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+
+		if ("writeToSession".equals(action)) {
+			String vorname = request.getParameter("vorname");
+			String nachname = request.getParameter("nachname");
+
+			HttpSession session = request.getSession();
+			session.setAttribute("vorname", vorname);
+			session.setAttribute("nachname", nachname);
+
+			out.println("<html><body>");
+			out.println("<h1>Schreiben von Daten in eine Sitzung</h1>");
+			out.println("<p>Sessiondaten wurden erfolgreich geschrieben!</p>");
+			out.println("<form method='get' action='SessionHandlingServlet'>");
+			out.println("<input type='hidden' name='action' value='readFromSession' />");
+			out.println("<input type='submit' value='Daten aus Session lesen' />");
+			out.println("</form>");
+			out.println("</body></html>");
+		} else if ("readFromSession".equals(action)) {
+			HttpSession session = request.getSession();
+			String vorname = (String) session.getAttribute("vorname");
+			String nachname = (String) session.getAttribute("nachname");
+
+			out.println("<html><body>");
+			out.println("<h1>Lesen von Daten aus einer Sitzung</h1>");
+			out.println("<p>Vorname: " + vorname + "</p>");
+			out.println("<p>Nachname: " + nachname + "</p>");
+			out.println("</body></html>");
 		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
